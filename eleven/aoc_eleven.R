77ec5f6edfd1228313c65089b9b79254f204cc1d
Jeffrey Brabec <@uvm.edu>
2021-12-11 10:11:57 -0500

    Day ten and eleven

--- eleven/aoc_eleven.R
+++ eleven/aoc_eleven.R
@@ -1,26 +1,60 @@
 #Advent of Code Day 11
 
 library(tidyverse)
+library(OpenImageR)
 library(here)
 #Part One
 
-input <- c("5483143223",
-           "2745854711",
-           "5264556173",
-           "6141336146",
-           "6357385478",
-           "4167524645",
-           "2176841721",
-           "6882881134",
-           "4846848554",
-           "5283751526")
+input <- read_lines(here("eleven/input.txt"))
 
 input_mat <- input %>% 
   map(str_split, "", simplify = TRUE) %>%
   map(as.integer) %>% 
   reduce(rbind) 
 
+kernel <- matrix(1:9 != 5, 3, 3)
+
+count_flashes(input_mat, iter = 750, part2 = TRUE)
+
 count_flashes <- function(x, iter = 100, part2 = FALSE) {
   
-  
+  count <- 0
+  for(i in 1:iter) {
+    
+    x <- step1(x)
+    nflashes <- sum(x == 0)
+    if(part2 & nflashes == prod(dim(x)))
+      return(i)
+    count <- count + nflashes
+    
+  }
+  if(part2)
+    warning('No synchronization detected in ', iter, ' steps')
+  count
+
+}
+
+step1 <- function(x) {
+  x <- x + 1
+  flashing <- flashed <- x == 10
+  while (any(flashing)) {
+    x <- x + OpenImageR::convolution(flashing, kernel)
+    flashing <- x > 9 & !flashed
+    flashed <- flashing | flashed
+  }
+  x[x > 9] <- 0
+  x
+}
+
+add_neighbours <- function(x) {
+  I <- nrow(x)
+  J <- ncol(x)
+  cbind(x[, -1], 0) +  # Right
+    rbind(x[-1, ], 0) +  # Down
+    cbind(0, x[, -J]) +  # Left
+    rbind(0, x[-I, ]) +  # Up
+    rbind(cbind(x[-1, -1], 0), 0) + # SE
+    rbind(0, cbind(x[-I, -1], 0)) + # NE
+    rbind(cbind(0, x[-1, -J]), 0) + # SW
+    rbind(0, cbind(0, x[-I, -J]))   # NW
 }
--- ten/aoc_ten.R
+++ ten/aoc_ten.R
@@ -5,22 +5,29 @@ library(here)
 
 #Part one
 
-input <- c("[({(<(())[]>[[{[]{<()<>>",
-           "[(()[<>])]({[<{<<[]>>(",
-           "{([(<{}[<>[]}>{[]{[(<()>",
-           "(((({<>}<{<{<>}{[]{[]{}",
-           "[[<[([]))<([[{}[[()]]]",
-           "[{[{({}]{}}([{[{{{}}([]",
-           "{<[[]]>}<{[{[{[]{()[[[]",
-           "[<(<(<(<{}))><([]([]()",
-           "<{([([[(<>()){}]>(<<{{",
-           "<{([{{}}[<[[[<>{}]]]>[]]") %>% 
-  as_tibble()
+lines <- read_lines(here("ten/input.txt"))
 
-str_detect(input$value, "\\(\\)|\\[\\]|\\{\\}|<>")
-str_detect(input$value, "\\(|\\[|\\{|<")
+old <- ''
+while (!identical(old, lines -> old))
+  lines <- gsub(r'(\(\)|<>|\{\}|\[\])', '', lines)
+illegals <- regmatches(lines, regexpr(r'(\)|>|\}|\])', lines))
 
-huh <- input %>% 
-  mutate(value = str_split(value, pattern = ""),
-         value = map())
-  filter()
+illegal_score <- c(')' = 3, ']' = 57, '}' = 1197, '>' = 25137)
+sum(illegal_score[illegals])
+
+#Part two
+#Doesn't actually matter what the closing bracket is you just need to score each of the open brackets.
+complete_score <- c('(' = 1, '[' = 2, '{' = 3, '<' = 4)
+
+autocomplete <- function(lines) {
+  old <- ''
+  while (!identical(old, lines -> old))
+    lines <- gsub(r'{\(\)|<>|\{\}|\[\]}', '', lines)
+  illegals <- grep(r'{\)|>|\}|\]}', lines)
+  chars <- strsplit(lines[-illegals], '')
+  scores <- sapply(chars, Reduce, init = 0, right = TRUE,
+                   f = function(c, s) 5 * s + complete_score[c])
+  median(scores)
+}
+
+autocomplete(lines)
